c2547cdc3b48d8bc22d1cc67151ea9c2c92e0569
raremiroir <miro.polfliet@student.kdg.be>
2023-08-31 17:18:18 +0200

    Add popup component

---
+++ src/lib/components/Utils/Popup.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { popup, type PopupSettings } from '@skeletonlabs/skeleton';
+
+   export let event: PopupSettings['event'] = 'click';
+   export let placement: PopupSettings['placement'] = 'bottom';
+
+   export let triggerClass: string = '';
+   let klass: string = '';
+   export { klass as class };
+
+   let popupComponent: PopupSettings;
+   $: popupComponent = { event, target: 'popupComponent', placement, closeQuery: '#closePopup' }
+</script>
+
+<button 
+   use:popup={popupComponent}
+   class="{triggerClass}"
+>
+   <slot name="trigger">Trigger</slot>
+</button>
+
+<div 
+   class="
+      w-fit h-fit p-2 variant-filled-surface 
+      !bg-white dark:!bg-slate-600
+      shadow-lg rounded-tile-xl {klass}"
+   data-popup="popupComponent">
+
+   <slot>Popup</slot>
+
+</div>
