75a01265664ad7c943bd3b8bb49587e1c97f44a7
raremiroir <miro.polfliet@student.kdg.be>
2023-08-31 22:32:38 +0200

    Add Icon component for alternating between iconify and animicon

---
+++ src/lib/components/Media/Icon.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+   import Iconify from "@iconify/svelte";
+   import AnimIcon from "./AnimIcon/AnimIcon.svelte";
+
+   export let icon: string = '';
+
+   let isAnimIcon: boolean = false;
+   $: isAnimIcon = icon.includes(':') ? false : true;
+
+   export let size: number = 70;
+   let klass: string = ''
+   export { klass as class };
+</script>
+
+{#if isAnimIcon}
+      <AnimIcon {icon} {size} />
+{:else}
+      <Iconify {icon} class="h-auto text-inherit {klass}" />
+{/if}
