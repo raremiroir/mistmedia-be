c692787d73b56c1a0b4b83ac4a80c43fedc3bcaa
raremiroir <miro.polfliet@student.kdg.be>
2023-09-01 15:41:54 +0200

    Add Svelte head

---
+++ src/lib/components/Core/Head.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+   // INIT SKELETON
+   import { storePopup, initializeStores } from "@skeletonlabs/skeleton";
+   initializeStores();
+   // INIT FLOATING UI
+   import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
+   storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
+
+</script>
+
+<svelte:head>
+
+</svelte:head>
+
